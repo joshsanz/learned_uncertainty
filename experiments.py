fbcb7d3c114f9df3559514d73c7b43ba39bfe605
Josh Sanz <jsanz@berkeley.edu>
2018-12-05 21:40:04 -0800

    Add linear time-varying gaussian model

--- experiments.py
+++ experiments.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('tkagg')
 from matplotlib import pyplot as plt
 
 from data_models import *
@@ -83,10 +85,64 @@ def run_gaussian_unbiased_covar(data, num_samples, num_assets, pred_params, cont
 
 def run_simple_gaussian_experiments():
     num_samples = 100
-    true_asset_value = np.array([0.0, 1.0, 1.1])
-    asset_covariance = np.diag( [1.0, 1.0, 0.1])
+    true_asset_value = np.array([0.8, 1.0, 1.1])
+    asset_covariance = np.diag( [0.02, 0.01, 0.03])
     data = get_gaussian_data(num_samples, true_asset_value, asset_covariance)
     num_assets = data.shape[1]
+
+    plt.plot(data, label='True Asset Values')
+    plt.legend()
+    plt.title('Input Data')
+    plt.show()
+
+    # Add experiments to run here.
+    experiments = [
+        ("gaussian_unbiased_covar", run_gaussian_unbiased_covar, None, {"gamma": 1}),
+        ("gaussian_unbiased_l1", run_gaussian_unbiased_norm, None, {"gamma": 1, "regularization": 1}),
+        ("gaussian_unbiased_l2", run_gaussian_unbiased_norm, None, {"gamma": 1, "regularization": 2}),
+    ]
+
+    bar_plot_mean = []
+    bar_plot_std = []
+    for name, experiment_func, pred_params, control_params in experiments:
+        predicted_asset_values, investment_strategies = experiment_func(data,
+                                                                        num_samples,
+                                                                        num_assets,
+                                                                        pred_params,
+                                                                        control_params)
+        predicted_return, true_return = get_returns(data, investment_strategies, predicted_asset_values)
+        print(name, np.sum(true_return))
+        bar_plot_mean.append(np.mean(true_return))
+        bar_plot_std.append(np.std(true_return))
+        # all_error = error(predicted_return, true_return)
+        # window = 10
+        # for i in range(0, num_samples-window, window):
+        #     print(name, np.mean(all_error[i:i + window]))
+        # We really just care about how well the investment strategies actually do,
+        # which is given by true_return.
+        plt.plot(np.arange(3, num_samples), true_return[3:], label=name + ' true return', alpha=0.33)
+        # In final plots, predicted return may not be relevant.
+        plt.plot(np.arange(3, num_samples), predicted_return[3:], label=name + ' predicted return')
+    plt.legend()
+    plt.show()
+
+    plt.bar(np.arange(len(experiments)), height=bar_plot_mean, yerr=bar_plot_std)
+    plt.show()
+
+
+def run_ltv_gaussian_experiments():
+    num_samples = 100
+    true_asset_v0 = np.array([0.9, 1.2, 1.0])
+    true_asset_delta = np.array([[0.002, -0.003, 0.001]])
+    asset_covariance = np.diag([1.0, 1.0, 0.2]) * 0.02
+    true_asset_value = true_asset_v0 + (true_asset_delta.T @ np.arange(0,num_samples).reshape(-1,1).T).T
+    data = get_gaussian_data(num_samples, np.zeros((3,)), asset_covariance) + true_asset_value
+    num_assets = data.shape[1]
+
+    plt.plot(data, label='True Asset Values')
+    plt.legend()
+    plt.title('Input Data')
+    plt.show()
 
     # Add experiments to run here.
     experiments = [
@@ -115,7 +171,7 @@ def run_simple_gaussian_experiments():
         # which is given by true_return.
         plt.plot(np.arange(3, num_samples), true_return[3:], label=name + ' true return', alpha=0.25)
         # In final plots, predicted return may not be relevant.
-        # plt.plot(np.arange(3, num_samples), predicted_return[3:], label=name + ' predicted return')
+        plt.plot(np.arange(3, num_samples), predicted_return[3:], label=name + ' predicted return')
     plt.legend()
     plt.show()
 
@@ -124,3 +180,4 @@ def run_simple_gaussian_experiments():
 
 if __name__ == "__main__":
     run_simple_gaussian_experiments()
+    run_ltv_gaussian_experiments()
