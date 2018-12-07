39509bae21767c3c0d7f9057f498e3b86b0021a3
Melih Elibol <elibol@gmail.com>
2018-12-07 06:45:19 -0500

    autoregression model.

--- prediction_models.py
+++ prediction_models.py
@@ -26,11 +26,11 @@ class UnbiasGaussianEstimator(PredictionModel):
     def sample_covariance(self, samples):
         return np.cov(samples, rowvar=False)
 
-    def predict(self, samples, assume_diag=True, p=1):
-        projected_means = np.empty(shape=(p, samples.shape[1]))
-        projected_covariance = np.empty(shape=(p, samples.shape[1], samples.shape[1]))
+    def predict(self, samples, assume_diag=True, n=1):
+        projected_means = np.empty(shape=(n, samples.shape[1]))
+        projected_covariance = np.empty(shape=(n, samples.shape[1], samples.shape[1]))
         projected_samples = samples
-        for i in range(p):
+        for i in range(n):
             projected_means[i] = self.sample_mean(projected_samples)
             covar = self.sample_covariance(projected_samples)
             if covar.shape == ():
@@ -40,55 +40,52 @@ class UnbiasGaussianEstimator(PredictionModel):
             projected_covariance[i, :, :] = covar
             projected_samples = np.vstack((projected_samples, projected_means[i]))
 
-        if p == 1:
+        if n == 1:
             return projected_means[0], projected_covariance[0]
         else:
             return projected_means, projected_covariance
 
 
-class OneDimensionalAutoRegression(PredictionModel):
+class AutoRegression(PredictionModel):
 
-    def __init__(self, p, learning_rate=0.001, seed=1):
-        super(PredictionModel, OneDimensionalAutoRegression).__init__(self)
-        self.r = np.random.RandomState(seed=seed)
+    def __init__(self, p, regularizer=0.001):
+        self.p = p
+        self.data = None
+        self.models = None
+        self.regularizer = regularizer
+
+    def fit(self, samples):
+        self.data = samples.T
+        self.models = []
+        for i in range(self.data.shape[0]):
+            model = OneDimensionalAutoRegression(self.p, self.regularizer)
+            model.fit(self.data[i])
+            self.models.append(model)
+
+    def predict(self, samples, n):
+        predictions = np.empty(shape=(n, samples.shape[1]))
+        for i in range(samples.shape[1]):
+            predictions[:, i] = self.models[i].predict(samples.T[i], n)
+        return predictions
+
+
+class OneDimensionalAutoRegression(object):
+
+    def __init__(self, p, regularizer=0.001):
         self.w = None
         self.p = p
-        self.samples = None
-        self.grad_loss = ag.grad(self.loss, 0)
-        self.learning_rate = learning_rate
-
-    def loss(self, w, r, p):
-        k = r.shape[0]
-        sum_outer = 0
-        for t in range(k):
-            sum_inner = - r[t]
-            for i in range(1, p):
-                if t - i < 0:
-                    continue
-                sum_inner += w[i] * r[t-i]
-            sum_inner = sum_inner**2
-            sum_outer += sum_inner
-        return sum_outer
+        self.regularizer = regularizer
 
     def fit(self, samples):
         assert(samples.shape[0] > self.p), "Number of samples are less than autoregression length"
         X = np.zeros([samples.shape[0]-self.p, self.p])
-        regularizer = 0.001
+        self.regularizer = 0.001
         for i in range(samples.shape[0] - self.p):
             X[i] = samples[i:i+self.p]
         y = samples[self.p:]
         self.w = np.matmul(\
-                np.linalg.inv(np.matmul(X.transpose(),X) + regularizer * np.identity(X.shape[1])),\
+                np.linalg.inv(np.matmul(X.transpose(),X) + self.regularizer * np.identity(X.shape[1])),\
                 np.matmul(X.transpose(),y))
-
-    #def toToepMat(x, L):
-    #    mat = np.zeros(x.shape[0]-L, L)
-    #    for i in range(L,x.shape[0]):
-    #
-    #def fit(self, samples):
-
-    #def predict(self, r):
-    #    return np.dot(r, self.w)
 
     def predict(self, r, n):
         plen = self.w.shape[0]
@@ -97,25 +94,41 @@ class OneDimensionalAutoRegression(PredictionModel):
         for i in range(n):
             pred[plen + i] = np.dot(pred[i:i+plen], self.w)
         return pred[-n:]
-        #for
 
 if __name__ == "__main__":
-    num_samples = 100
-    sine_samples = np.sin(np.linspace(0, 10 * np.pi, num_samples))
-    reg_len = 20
+    from data_models import GaussianNoise
 
-    ar = OneDimensionalAutoRegression(reg_len)
-    ar.fit(sine_samples)
-
-    pred_sine = [0 for i in range(len(sine_samples))]
-    pred_sine = ar.predict(sine_samples, 80)
-    plt.plot(np.arange(num_samples), sine_samples)
-    plt.plot(np.arange(reg_len,num_samples), pred_sine, linewidth = 10, alpha = 0.5)
-    plt.savefig('test_plots/autograd_test.png')
-    #plt.show()
-
-    # from data_models import GaussianNoise
+    # num_samples = 100
+    # sine_samples = np.sin(np.linspace(0, 10 * np.pi, num_samples))
+    # reg_len = 20
     #
+    # ar = OneDimensionalAutoRegression(reg_len)
+    # ar.fit(sine_samples)
+    #
+    # num_project = 100
+    # pred_sine = ar.predict(sine_samples, num_project)
+    # plt.plot(np.arange(num_samples), sine_samples)
+    # plt.plot(np.arange(num_samples-1, num_samples+num_project-1), pred_sine, linewidth = 10, alpha = 0.5)
+    # plt.show()
+
+    num_samples = 1000
+    num_assets = 3
+
+    mu_truth = np.ones(num_assets)
+    sigma_truth = np.diag([0.5, 0.3, 0.2])
+    sampler = GaussianNoise()
+
+    data = np.zeros(shape=(num_samples, num_assets))
+    for i in range(num_samples):
+        data[i] = sampler.sample((mu_truth, sigma_truth))
+
+    L = 3
+    ar = AutoRegression(L)
+    ar.fit(data)
+    prediction = ar.predict(data, L)
+    for l in range(L):
+        print("\n prediction", l, prediction[l])
+
     # num_samples = 1000
     # num_assets = 3
     #
