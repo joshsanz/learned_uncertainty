9e20b3c11f7ff7973890c0119833c411f6df220a
Josh Sanz <jsanz@berkeley.edu>
2018-12-07 16:41:31 -0800

    Add S&P500 symbols

--- alpha_vantage_data/fetch_real_data.py
+++ alpha_vantage_data/fetch_real_data.py
@@ -10,12 +10,13 @@ import time
 
 
 # Documentation: https://www.alphavantage.co/documentation/
-api_key = "QCI3RT39F1VOUR21"
+api_key_1 = "QCI3RT39F1VOUR21"
+api_key_2 = "BVS30FU4LEK4Q948"
 tech_stocks = ["MSFT", "AAPL", "GOOGL", "AMZN", "FB", "NFLX", "IBM", "TSLA"]
 DJIA = ["MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DIS", "DWDP", "XOM",
         "GS", "HD", "IBM", "INTC", "JNJ", "JPM", "MCD", "MRK", "MSFT", "NKE", "PFE",
         "PG", "TRV", "UTX", "UNH", "VZ", "V", "WMT", "WBA"]
-SP500 = ["AAPL", "ABBV", "ABT", "ACN", "AGN", "AIG", "ALL", "AMGN", "AMZN", "AXP", "BA", "BAC", "BIIB",
+SP100 = ["AAPL", "ABBV", "ABT", "ACN", "AGN", "AIG", "ALL", "AMGN", "AMZN", "AXP", "BA", "BAC", "BIIB",
         "BK", "BKNG", "BLK", "BMY", "BRK.B", "C", "CAT", "CELG", "CHTR", "CL", "CMCSA", "COF", "COP",
         "COST", "CSCO", "CVS", "CVX", "DHR", "DIS", "DUK", "DWDP", "EMR", "EXC", "F", "FB", "FDX", "FOX",
         "FOXA", "GD", "GE", "GILD", "GM", "GOOG", "GOOGL", "GS", "HAL", "HD", "HON", "IBM", "INTC", "JNJ",
@@ -23,8 +24,45 @@ SP500 = ["AAPL", "ABBV", "ABT", "ACN", "AGN", "AIG", "ALL", "AMGN", "AMZN", "AXP
         "MRK", "MS", "MSFT", "NEE", "NFLX", "NKE", "NVDA", "ORCL", "OXY", "PEP", "PFE", "PG", "PM", "PYPL",
         "QCOM", "RTN", "SBUX", "SLB", "SO", "SPG", "T", "TGT", "TXN", "UNH", "UNP", "UPS", "USB", "UTX",
         "V", "VZ", "WBA", "WFC", "WMT", "XOM"]
+SP500 = ["A", "AAL", "AAP", "AAPL", "ABBV", "ABC", "ABMD", "ABT", "ACN", "ADBE", "ADI", "ADM", "ADP", "ADS",
+        "ADSK", "AEE", "AEP", "AES", "AFL", "AGN", "AIG", "AIV", "AIZ", "AJG", "AKAM", "ALB", "ALGN", "ALK",
+        "ALL", "ALLE", "ALXN", "AMAT", "AMD", "AME", "AMG", "AMGN", "AMP", "AMT", "AMZN", "ANET", "ANSS",
+        "ANTM", "AON", "AOS", "APA", "APC", "APD", "APH", "APTV", "ARE", "ARNC", "ATVI", "AVB", "AVGO",
+        "AVY", "AWK", "AXP", "AZO", "BA", "BAC", "BAX", "BBT", "BBY", "BDX", "BEN", "BF", "BHF", "BHGE",
+        "BIIB", "BK", "BKNG", "BLK", "BLL", "BMY", "BR", "BRK", "BSX", "BWA", "BXP", "C", "CAG", "CAH",
+        "CAT", "CB", "CBOE", "CBRE", "CBS", "CCI", "CCL", "CDNS", "CELG", "CERN", "CF", "CFG", "CHD", "CHRW",
+        "CHTR", "CI", "CINF", "CL", "CLX", "CMA", "CMCSA", "CME", "CMG", "CMI", "CMS", "CNC", "CNP", "COF",
+        "COG", "COO", "COP", "COST", "COTY", "CPB", "CPRT", "CRM", "CSCO", "CSX", "CTAS", "CTL", "CTSH",
+        "CTXS", "CVS", "CVX", "CXO", "D", "DAL", "DE", "DFS", "DG", "DGX", "DHI", "DHR", "DIS", "DISCA",
+        "DISCK", "DISH", "DLR", "DLTR", "DOV", "DRE", "DRI", "DTE", "DUK", "DVA", "DVN", "DWDP", "DXC", "EA",
+        "EBAY", "ECL", "ED", "EFX", "EIX", "EL", "EMN", "EMR", "EOG", "EQIX", "EQR", "ES", "ESRX", "ESS",
+        "ETFC", "ETN", "ETR", "EVRG", "EW", "EXC", "EXPD", "EXPE", "EXR", "F", "FANG", "FAST", "FB", "FBHS",
+        "FCX", "FDX", "FE", "FFIV", "FIS", "FISV", "FITB", "FL", "FLIR", "FLR", "FLS", "FLT", "FMC", "FOX",
+        "FOXA", "FRT", "FTI", "FTNT", "FTV", "GD", "GE", "GILD", "GIS", "GLW", "GM", "GOOG", "GOOGL",
+        "GPC", "GPN", "GPS", "GRMN", "GS", "GT", "GWW", "HAL", "HAS", "HBAN", "HBI", "HCA", "HCP", "HD",
+        "HES", "HFC", "HIG", "HII", "HLT", "HOG", "HOLX", "HON", "HP", "HPE", "HPQ", "HRB", "HRL", "HRS",
+        "HSIC", "HST", "HSY", "HUM", "IBM", "ICE", "IDXX", "IFF", "ILMN", "INCY", "INFO", "INTC", "INTU",
+        "IP", "IPG", "IPGP", "IQV", "IR", "IRM", "ISRG", "IT", "ITW", "IVZ", "JBHT", "JCI", "JEC", "JEF",
+        "JKHY", "JNJ", "JNPR", "JPM", "JWN", "K", "KEY", "KEYS", "KHC", "KIM", "KLAC", "KMB", "KMI",
+        "KMX", "KO", "KORS", "KR", "KSS", "KSU", "L", "LB", "LEG", "LEN", "LH", "LIN", "LKQ", "LLL",
+        "LLY", "LMT", "LNC", "LNT", "LOW", "LRCX", "LUV", "LW", "LYB", "M", "MA", "MAA", "MAC", "MAR",
+        "MAS", "MAT", "MCD", "MCHP", "MCK", "MCO", "MDLZ", "MDT", "MET", "MGM", "MHK", "MKC", "MLM",
+        "MMC", "MMM", "MNST", "MO", "MOS", "MPC", "MRK", "MRO", "MS", "MSCI", "MSFT", "MSI", "MTB",
+        "MTD", "MU", "MXIM", "MYL", "NBL", "NCLH", "NDAQ", "NEE", "NEM", "NFLX", "NFX", "NI", "NKE", "NKTR",
+        "NLSN", "NOC", "NOV", "NRG", "NSC", "NTAP", "NTRS", "NUE", "NVDA", "NWL", "NWS", "NWSA", "O",
+        "OKE", "OMC", "ORCL", "ORLY", "OXY", "PAYX", "PBCT", "PCAR", "PCG", "PEG", "PEP", "PFE", "PFG",
+        "PG", "PGR", "PH", "PHM", "PKG", "PKI", "PLD", "PM", "PNC", "PNR", "PNW", "PPG", "PPL", "PRGO",
+        "PRU", "PSA", "PSX", "PVH", "PWR", "PXD", "PYPL", "QCOM", "QRVO", "RCL", "RE", "REG", "REGN",
+        "RF", "RHI", "RHT", "RJF", "RL", "RMD", "ROK", "ROL", "ROP", "ROST", "RSG", "RTN", "SBAC", "SBUX",
+        "SCG", "SCHW", "SEE", "SHW", "SIVB", "SJM", "SLB", "SLG", "SNA", "SNPS", "SO", "SPG", "SPGI", "SRE",
+        "STI", "STT", "STX", "STZ", "SWK", "SWKS", "SYF", "SYK", "SYMC", "SYY", "T", "TAP", "TDG", "TEL",
+        "TGT", "TIF", "TJX", "TMK", "TMO", "TPR", "TRIP", "TROW", "TRV", "TSCO", "TSN", "TSS", "TTWO",
+        "TWTR", "TXN", "TXT", "UA", "UAA", "UAL", "UDR", "UHS", "ULTA", "UNH", "UNM", "UNP", "UPS", "URI",
+        "USB", "UTX", "V", "VAR", "VFC", "VIAB", "VLO", "VMC", "VNO", "VRSK", "VRSN", "VRTX", "VTR",
+        "VZ", "WAT", "WBA", "WCG", "WDC", "WEC", "WELL", "WFC", "WHR", "WLTW", "WM", "WMB", "WMT", "WRK",
+        "WU", "WY", "WYNN", "XEC", "XEL", "XLNX", "XOM", "XRAY", "XRX", "XYL", "YUM", "ZBH", "ZION", "ZTS", ]
 
-stocks = DJIA
+stocks = SP100
 
 def daily_value_estimate(daily_value_dict):
     open_val = float(daily_value_dict["1. open"])
@@ -35,7 +73,7 @@ def daily_value_estimate(daily_value_dict):
     return close_val
 
 
-def get_time_series(symbol):
+def get_time_series(symbol, api_key):
     url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + symbol + "&apikey=" + api_key
     htmltext = request.urlopen(url)
     content = htmltext.read()
@@ -51,14 +89,15 @@ def get_time_series(symbol):
             result[i] = daily_value_estimate(time_dict[dates[i]])
     except Exception as e:
         print(symbol, "failed:", e, url)
-    # Throttled at 5 requests per minute.
-    time.sleep(12.0)
     return dates, result
 
 all_data = []
 all_dates = []
-for stock in stocks:
-    single_dates, single_data = get_time_series(stock)
+keys = [api_key_1, api_key_2] * len(stocks)
+for i, stock in enumerate(stocks):
+    single_dates, single_data = get_time_series(stock, keys[i])
+    # Throttled to 5 api calls per minutes per key, split between two keys (shhhh)
+    time.sleep(12)
     print(stock, single_data.shape)
     all_data.append(single_data)
     all_dates.append(single_dates)
